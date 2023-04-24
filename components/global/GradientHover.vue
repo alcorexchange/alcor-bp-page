a5477e7c87432ce116c363f2883b748cfda6fd40
sa8ab <bwsaeed8@gmail.com>
2023-04-24 21:00:04 +0430

    ^ hover effect on products

--- components/global/GradientHover.vue
+++ components/global/GradientHover.vue
@@ -2,8 +2,12 @@
   <div class="gradient-hover">
     <div class="image" v-if="image">
       <img :src="image" alt="" />
+      <div class="image-overlay"></div>
     </div>
-    <div class="effects"></div>
+    <div class="effects">
+      <div class="effect effect-1"></div>
+      <div class="effect effect-2"></div>
+    </div>
     <div class="slot">
       <slot></slot>
     </div>
@@ -19,6 +23,7 @@ const image = useAsset(props.image || "");
 
 <style scoped lang="scss">
 .gradient-hover {
+  --duration: calc(var(--r-duration) * 2.4);
   display: flex;
   flex-direction: column;
   position: relative;
@@ -27,18 +32,61 @@ const image = useAsset(props.image || "");
   background: color(b3);
   border: 1px solid color(border-color);
   border-radius: var(--r-radius-2);
+  cursor: pointer;
+  overflow: hidden;
+  transition: all var(--duration);
   .image {
     height: 200px;
+    position: relative;
     img {
       object-fit: contain;
     }
+    .image-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(to top, color(b3), transparent);
+    }
   }
   .effects {
-    position: absolute;
-    z-index: 0;
+    z-index: 2;
+    pointer-events: none;
+    .effect {
+      position: absolute;
+      width: 298px;
+      height: 260px;
+      filter: blur(275px);
+      opacity: 0;
+      transition: all var(--duration);
+      &-1 {
+        top: 100%;
+        left: 100%;
+        background: color();
+      }
+      &-2 {
+        top: 100%;
+        left: 0;
+        transform: translateX(-50%);
+        background-color: color(blue);
+      }
+    }
   }
   .slot {
-    z-index: 2;
+    position: relative;
+    z-index: 4;
+  }
+  &:hover {
+    transform: scale(1.04);
+    .effect {
+      opacity: 1;
+      &-1 {
+        top: 0;
+        left: 90%;
+        transform: translateY(-80%);
+      }
+    }
   }
 }
 </style>
