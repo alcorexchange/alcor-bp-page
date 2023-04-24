be5acb1e8a6399f140adc99be16686e7fbd66c9f
sa8ab <bwsaeed8@gmail.com>
2023-04-24 20:25:34 +0430

    ^

--- components/home/HomeProductItem.vue
+++ components/home/HomeProductItem.vue
@@ -1,7 +1,8 @@
 <template>
   <GradientHover class="home-product-item" image="/images/product-image-2.png">
-    <div class="title">some title</div>
-    <div class="description">some description</div>
+    <div class="title">Alcor Markets</div>
+    <Spacer size="low" />
+    <div class="description">Decentralized orderbook markets on WAX, EOS, Proton and Telos</div>
   </GradientHover>
 </template>
 
@@ -10,3 +11,14 @@ defineProps<{
   image?: string;
 }>();
 </script>
+
+<style scoped lang="scss">
+.title {
+  font-size: var(--r-font-xlarge);
+  font-weight: bold;
+}
+.description {
+  font-size: var(--r-font-xlarge);
+  color: color(text-3);
+}
+</style>
